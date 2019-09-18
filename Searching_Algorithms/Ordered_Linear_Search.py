670a6a1e3e1ec5c15c3cde10a1b2c6c9e1756612
lakshika <lakshika@localhost.localdomain>
2019-09-18 07:21:39 +0530

    deleted folder Searching Algorithm

--- Searching_Algorithms/Ordered_Linear_Search.py
+++
@@ -1,9 +0,0 @@
-def ordered_linear_search(arr,value):
-	for i in range(len(arr)):
-		if arr[i]==value:
-			return i
-		elif arr[i]>value:
-			return -1
-	return -1
-A=[34,48,52,57,60,68,77,102]
-print(ordered_linear_search(A,60))
--- Searching_Algorithms/Unordered_Linear_Search.py
+++
@@ -1,8 +0,0 @@
-def un_ordered_linear_search(arr,value):
-	for i in range(len(arr)):
-		if arr[i]==value:
-			return i
-	return -1
-
-A=[534,246,933,127,277,321,454,798,277]
-print(un_ordered_linear_search(A,277))
